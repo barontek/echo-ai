01a26f521cc983387055300db60ac0d5213b8ff9
barontek <ahmetysuf9183@hotmail.com>
2026-04-05 00:13:42 +0300

    Fix unused useState import in MessageList

--- frontend/src/components/MessageList.tsx
+++ frontend/src/components/MessageList.tsx
@@ -1,4 +1,4 @@
-import { memo, useEffect, useRef, useState } from 'react';
+import { memo, useEffect, useRef } from 'react';
 import ReactMarkdown from 'react-markdown';
 import { useChat } from '../context';
 
